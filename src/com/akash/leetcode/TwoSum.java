18213e5f6c4c554850c2afb87d3c010bf6fe4718
mrakashkumar <akash@Akashs-MacBook-Pro.local>
2025-02-25 23:44:20 +0530

    first branch

---
+++ src/com/akash/leetcode/TwoSum.java
@@ -0,0 +1,11 @@
+package com.akash.leetcode;
+/*
+ * https://leetcode.com/problems/two-sum/description/
+ */
+public class TwoSum {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
