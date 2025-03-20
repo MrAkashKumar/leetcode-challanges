2b4ec3317acde362a004d95600741b005d62210d
mrakashkumar <akash@Akashs-MacBook-Pro.local>
2025-03-20 23:31:34 +0530

    3 sum code fixed

---
+++ src/com/akash/leetcode/ThreeSum.java
@@ -0,0 +1,46 @@
+package com.akash.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/*
+ * https://leetcode.com/problems/3sum/description/
+ */
+public class ThreeSum {
+
+    public static void main(String[] args) {
+        int[] nums = {-1,0,1,2,-1,-4};
+        List<List<Integer>> lists = threeSum(nums);
+        System.out.println(lists);
+    }
+
+    
+    private static List<List<Integer>> threeSum(int[] nums){
+        int target = 0;
+        Arrays.sort(nums);
+        Set<List<Integer>> s = new HashSet<>();
+        List<List<Integer>> output = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++){
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target) {
+                    s.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                } else if (sum < target) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        output.addAll(s);
+        return output;
+    }
+
+}
