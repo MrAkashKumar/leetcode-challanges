d1f402578ef5ef11d16197743a0dd5cef57e701d
mrakashkumar <akash@Akashs-MacBook-Pro.local>
2025-03-23 23:54:31 +0530

    most container water

---
+++ src/com/akash/leetcode/ContainerWithMostWater.java
@@ -0,0 +1,20 @@
+package com.akash.leetcode;
+/*
+ * https://leetcode.com/problems/container-with-most-water/description/
+ * Topics - Array, Two-pointer
+ */
+public class ContainerWithMostWater {
+
+    public static void main(String[] args) {
+        int[] height = {};
+        int maxAmountOfWater = maxArea(height);
+        System.out.println(maxAmountOfWater);
+    }
+
+    private static int maxArea(int[] height){
+
+
+        return 0;
+    }
+
+}
