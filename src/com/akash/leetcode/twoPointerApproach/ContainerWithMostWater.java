a5292cb481d8cfc522252efa4280fe0254a6ada8
mrakashkumar <akash@Akashs-MacBook-Pro.local>
2025-03-28 20:12:49 +0530

    conatiner with most water

---
+++ src/com/akash/leetcode/twoPointerApproach/ContainerWithMostWater.java
@@ -0,0 +1,57 @@
+package com.akash.leetcode.twoPointerApproach;
+/*
+ * https://leetcode.com/problems/container-with-most-water/description/
+ * Topics - Array, Two-pointer
+ */
+public class ContainerWithMostWater {
+
+    public static void main(String[] args) {
+        int[] height = {1,1};
+        int maxAmountOfWater = maxAreaWater(height);
+        //System.out.println(maxAmountOfWater);
+        int maxAreaOfWater = maxAreaViaBruteApproach(height);
+        System.out.println(maxAreaOfWater);
+    }
+
+    /*
+     * two pointer approach
+     * time complexitity - o(n)
+     * space complexity - o(1)
+     * / depending on minimum height we will move points - we will chose minimum height
+     */
+    private static int maxAreaWater(int[] height){
+        int left = 0; int right = height.length-1;
+        int maxWaterArea = 0;
+        while (left <= right) {
+            /*find the maximum area of water in container */
+            int area = Math.min(height[left], height[right]) * (right -left);
+            Math.max(maxWaterArea, area);
+
+            if(height[left] < height[right]){
+                left++;
+            }else{
+                right --;
+            }
+            
+        }
+        return maxWaterArea;
+    }
+
+    /*
+     * brute approach technique
+     * time complexity - o(square of n)
+     * space complexity - o(1)
+     */
+    private static int maxAreaViaBruteApproach(int[] height){
+        int length = height.length;
+        int maxWaterArea = 0;
+        for(int i = 0; i< length; i++){
+            for(int j = i+1; j< length; j++){
+                int area = Math.min(height[i], height[j]) * (j-i);
+                maxWaterArea = Math.max(maxWaterArea, area); // update max Water Area
+            }
+        }
+        return maxWaterArea;
+    }
+
+}
