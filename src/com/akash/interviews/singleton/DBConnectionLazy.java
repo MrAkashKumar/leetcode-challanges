7aaef2bd0b21ea1fe1b62ea172a2e386ede98ad4
mrakashkumar <akash@Akashs-MacBook-Pro.local>
2025-03-29 23:06:06 +0530

    added singleton types and way

---
+++ src/com/akash/interviews/singleton/DBConnectionLazy.java
@@ -0,0 +1,14 @@
+package com.akash.interviews.singleton;
+/*
+ * Singleton class
+ * Lazy Initialization
+ */
+public class DBConnectionLazy {
+    
+    private static DBConnectionLazy dbConnection;
+
+    private DBConnectionLazy(){
+        
+    }
+
+}
