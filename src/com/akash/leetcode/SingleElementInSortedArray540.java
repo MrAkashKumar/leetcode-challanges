37dcb1e4317f64829b3a8528d0000619473d90fa
mrakashkumar <akash@Akashs-MacBook-Pro.local>
2025-04-22 23:55:35 +0530

    added  multithreaded

---
+++ src/com/akash/leetcode/SingleElementInSortedArray540.java
@@ -0,0 +1,34 @@
+package com.akash.leetcode;
+
+/*
+ * https://leetcode.com/problems/single-element-in-a-sorted-array/description/
+ * solve via binary search
+ * 
+ */
+public class SingleElementInSortedArray540 {
+
+    public static void main(String[] args) {
+        
+        /*
+         * {1,1,2,3,3,4,4,8,8}
+         */
+        int[] num = {1,1,2,3,3,4,4,8,8};
+        int uniqueElement = findUniqueNumberWithSortedArray(num);
+        System.out.println(uniqueElement);
+    }
+
+    /*
+     * 
+     * solve via binary search
+     * Time complexity - o(log n)
+     * Space complexity - o(1)
+     * 
+     */
+    private static int findUniqueNumberWithSortedArray(int[] num){
+        
+        
+        
+        return 0;
+    }
+    
+}
