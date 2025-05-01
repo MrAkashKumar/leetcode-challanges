09622b3547339ca95d8ebe170e759498bda22f18
mrakashkumar <akash@Akashs-MacBook-Pro.local>
2025-05-01 22:59:43 +0530

    excel & exception sheet

---
+++ src/com/akash/basic/oops/Runner1.java
@@ -0,0 +1,30 @@
+package com.akash.basic.oops;
+
+public class Runner1 {
+
+    public static void main(String[] args) throws Exception{
+        //Child obj3 = new Person();
+        //obj3.display(); // error
+
+		Person p = new Child();
+		p.display(); // child.Display() called}
+    }
+
+     static public class Person{
+
+        public void display(){
+            System.out.println("Person.Display() called");
+        }
+    }
+    
+    static class Child extends Person{
+
+        public void display(){
+            System.out.println("Child.Display() called");
+        }
+    }
+    
+    
+}
+
+
